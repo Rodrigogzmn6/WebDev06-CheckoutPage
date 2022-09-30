b969ba1a049bf1b4ad27b1e45f6ada2abd374aac
Rodrigo Guzman <rodrigoguzman6796@gmail.com>
2022-09-30 13:16:33 -0300

    Project finished

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 const quantityButton = $(".item-quantity button");
+const submmitButton = $("#contact-address button");
 
 let shipping = 19;
 let total = 0;
+let alertMessage = "Thank you for buying :)";
 
 //Add event to all buttons and change quantity in page
 quantityButton.on("click", function() {
@@ -23,7 +25,6 @@ quantityButton.on("click", function() {
 
 //GetTotalAmount
 getTotalAmount();
-
 
 function getTotalAmount() {
     let totalItemsAmount = 0;
@@ -50,4 +51,78 @@ function getTotalAmount() {
 function setTotalText() {
     let totalAmountString =  "$" + (total).toString();
     $("#final-amount").html(totalAmountString);
+}
+
+submmitButton.on('click', function(){
+    validateForm();
+});
+
+function validateForm() {
+    let formFields = [];
+    let isFormValid = true;
+
+    //Get all the form fields
+    $.each($("#contact-address form input"), (i, formField) => {
+        if(formField.type !== "checkbox") {
+            formFields.push(formField);
+        }
+    });
+    formFields.push($("#contact-address form select")[0]);
+
+    //Check fields validation
+    $.each(formFields, (i, formField) => {
+        if(formField.type === "email") {
+            if(!checkEmail(formField)) {
+                isFormValid = checkEmail(formField);
+                return;
+            }
+        } else if(formField.type === "tel") {
+            if(!checkTel(formField)) {
+                isFormValid = checkTel(formField);
+                return;
+            }
+        } else {
+            if(!checkText(formField)) {
+                isFormValid = checkText(formField);
+                return;
+            }
+        }
+    });
+
+    alert(alertMessage);
+}
+
+function checkEmail(formField) {
+    let atPos = formField.value.indexOf("@");
+    let dotPos = formField.value.lastIndexOf(".");
+    let valid = 
+        atPos > -1 &&
+        dotPos > atPos &&
+        formField.value !== "";
+
+    if (!valid) {
+        alertMessage = "Please check your email address";
+    } 
+
+    return valid;
+}
+
+function checkTel(formField) {
+    let valid = /^[0-9]+$/.test(formField.value) && formField.value !== "";
+
+    if (!valid) {
+        alertMessage = "Please check your phone number";
+    } 
+    
+    return valid;
+}
+
+function checkText(formField) {
+    let valid = formField.value !== "";
+
+    if (!valid) {
+        alertMessage = "Please fill all the form fields to continue";
+    } 
+    
+    return (valid);
 }
